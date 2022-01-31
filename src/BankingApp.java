b1aaa27c9ef0cbdaa92e9903464694621ca5018a
TP-Edureka <tpdawnsun@gmail.com>
2022-01-31 20:42:59 +0200

    Added main method

--- src/BankingApp.java
+++ src/BankingApp.java
@@ -27,7 +27,40 @@ class BankDetails {
         System.out.println("Account type: " + acc_type);
         System.out.println("Balance is: " + balance);
     }
+
+    //Method to deposit money
+    public void deposit() {
+        long amt;
+
+        System.out.println("Enter amount you want to deposit");
+
+        amt = sc.nextLong();
+        balance = balance + amt;
+    }
 }
 
 public class BankingApp {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+
+        //create initial accounts
+        System.out.println("How many number of customers do you want to input? ");
+        int n = sc.nextInt();
+        BankDetails C[] = new BankDetails[n];
+        for(int i = 0; i < C.length; i++) {
+            C[i] = new BankDetails();
+            C[i] = new BankDetails();
+            C[i].openAccount();
+        }
+
+        //loop runs until number 5 is not pressed to exit
+        int ch;
+
+        do {
+
+            System.out.println("\n*** Banking System Application");
+            System.out.println("1. Display all account details\n 2. Search by Account number\n 3. Deposit the amount \n 4. Withdraw the amount\n 5.Exit ");
+            System.out.print("Enter your choice");
+        }
+    }
 }
